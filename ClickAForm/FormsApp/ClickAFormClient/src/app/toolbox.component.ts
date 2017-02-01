5c8cd582770fbc3b7a144123c43e523cfc990aac
chengor <chen.goren94@gmail.com>
2017-02-01 17:26:30 +0200

    Insert first angular2 app and client

---
+++ ClickAForm/FormsApp/ClickAFormClient/src/app/toolbox.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'toolbox',
+  templateUrl: './toolbox.component.html',
+  styleUrls: ['./toolbox.component.css']
+})
+
+export class ToolBoxComponent {
+}
